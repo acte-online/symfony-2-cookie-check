fc2018139f61c0ba808d009b8206b79eef1fcf5f
acte-online.com <contact@acte-online.com>
2015-10-05 16:01:49 +0200

    Update CookieCheckController.php

--- src/BUNDLE/SiteBundle/Controller/CookieCheckController.php
+++ src/BUNDLE/SiteBundle/Controller/CookieCheckController.php
@@ -1,1 +1,20 @@
 <?php
+namespace BUNDLE\SiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PageSiteController extends Controller
+{
+  public function indexAction()
+  {
+    //On créé le cookie de check
+    if (isset($_COOKIE['site_cookie_check'])){
+    $cookie = "";
+    }
+    else{
+    $cookie = setcookie('site_cookie_check', 'web-site.com', time() + 365*24*3600, '/', null, false, true); 
+    }
+    
+    return $this->render('BUNDLESiteBundle:Page:index.html.twig', array('cookie' => $cookie,));
+  }
+}

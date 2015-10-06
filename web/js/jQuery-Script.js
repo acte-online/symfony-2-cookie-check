43f1c22c41ba50f718f8e06effb35d98692af8e8
acte-online <contact@acte-online.com>
2015-10-06 07:49:06 +0200

    Update jQuery-Script.js

--- web/js/jQuery-Script.js
+++ web/js/jQuery-Script.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
+	$('#cookie-div:hidden').fadeIn(1000);
 	$('#cookie-check-bt').click(function(){$('#cookie-div').fadeOut(1000);});
-	$('#cookie-div:hidden').fadeIn(1000); 
 });
